e5a489275989c66f74c5b58669113b97567c4d96
DESKTOP-B3V34VL\calixto <nicolas3jdd@gmail.com>
2026-07-17 00:20:34 +0200

    cors

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ app.use(cors({
     }
   },
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
+  allowedHeaders: ['Content-Type', 'Authorization','Cache-Control', 'Pragma'],
   credentials: true
 }));
 
