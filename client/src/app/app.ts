c2d38f283aee3a05ccd7617384e860c7ecf9419c
DESKTOP-B3V34VL\calixto <nicolas3jdd@gmail.com>
2026-07-21 12:45:53 +0200

    responsiveApp

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -7,6 +7,20 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('client');
+export class App implements OnInit{
+  protected readonly title = signal('ManagerPro');
+
+  async ngOnInit() {
+    this.forzarHorizontal();
+  }
+
+  async forzarHorizontal() {
+    try {
+      if (screen.orientation && (screen.orientation as any).lock) {
+        await (screen.orientation as any).lock('landscape');
+      }
+    } catch (err) {
+      console.log('No se pudo bloquear la orientación:', err);
+    }
+  }
 }
